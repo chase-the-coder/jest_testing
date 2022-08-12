79893a58fccf9094ca8d68a79fa19ccc41104e56
Chase <chase.lindsay1@gmail.com>
2022-08-11 19:46:38 -0500

    first repo push

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import Output from './Output'
+const Greeting = () => {
+  const [changedText, setChangedText] = useState(false)
+  const HandleButtonClick = () => {
+    setChangedText(true)
+  }
+  return (
+    <div>
+        <h2>Hello World!</h2>
+        {!changedText && <Output>It's good to see you!</Output>}
+        {changedText && <Output>Changed!</Output>}
+        <button onClick={HandleButtonClick}>Change Text!</button>
+    </div>
+
+  )
+}
+
+export default Greeting
